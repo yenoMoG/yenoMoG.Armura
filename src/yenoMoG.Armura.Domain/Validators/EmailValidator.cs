226305e784aef464a544bb30645a981831e1fede
Livia Sayuri Souza <livia.souza@easynvest.com.br>
2020-04-06 15:46:31 -0300

    Adiciona validador de email

---
+++ src/yenoMoG.Armura.Domain/Validators/EmailValidator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Net.Mail;
+
+namespace yenoMoG.Armura.Domain.Validators
+{
+	public static class EmailValidator
+	{
+		public static bool IsEmailValid(this string emailAddress)
+		{
+			try
+			{
+				var email = new MailAddress(emailAddress);
+
+				return true;
+			}
+			catch (Exception)
+			{
+				return false;
+			}
+		}
+	}
+}
