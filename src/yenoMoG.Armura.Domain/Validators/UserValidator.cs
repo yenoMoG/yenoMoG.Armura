54d256243268f21a797ccc3cd4ab5de8167b6fbe
Livia Sayuri Souza <livia.souza@easynvest.com.br>
2020-04-20 15:18:03 -0300

    Adiciona testes

--- src/yenoMoG.Armura.Domain/Validators/UserValidator.cs
+++ src/yenoMoG.Armura.Domain/Validators/UserValidator.cs
@@ -4,18 +4,19 @@ using yenoMoG.Armura.Domain.Models;
 
 namespace yenoMoG.Armura.Domain.Validators
 {
-    internal class UserValidator : AbstractValidator<User>
+	internal class UserValidator : AbstractValidator<User>
 	{
 		private const int MINIMUM_AGE = 18;
 		internal UserValidator()
 		{
-            RuleFor(u => u.Cpf).Must(cpf => cpf.IsCpfValid()).WithMessage("O CPF é invalido");
-			RuleFor(u => u.Name.Length).GreaterThanOrEqualTo(5).WithMessage("Nome invalido");
-			RuleFor(u => u.Name).NotEmpty().Matches(@"^((\b[A-zÀ-ú']{2,40}\b)\s*){2,}$").WithMessage("Nome invalido");
+			RuleFor(u => u.Cpf).Must(cpf => cpf.IsCpfValid()).WithMessage("O CPF é invalido");
+            RuleFor(u => u.Name)
+                .NotEmpty().WithErrorCode("NameIsNotNullOrEmpty").WithMessage("O nome deve ser preenchido")
+				.Matches(@"^((\b[A-zÀ-ú']{2,40}\b)\s*){2,}$").WithErrorCode("NameRegexInvalid").WithMessage("Nome deve ser completo");
 			RuleFor(u => u.Nickname).NotEmpty().WithMessage("Apelido não pode ser vazio");
 			RuleFor(u => u.Email).Must(email => email.IsEmailValid()).WithMessage("Precisa conter email válido");
-			RuleFor(u => u.Password.Length).GreaterThanOrEqualTo(8).LessThanOrEqualTo(16).WithMessage("Senha precisa ser maior que 8 e menor que 16");
-			RuleFor(u => u.Password).NotEmpty().Matches(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})").WithMessage("Senha precisa ser válida");
+			RuleFor(u => u.Password).NotEmpty().WithErrorCode("PasswordIsNotNullOrEmpty").WithMessage("A senha deve ser preenchida")
+				.Matches(@"(?=^.{8,}$)((?=.*\d)(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$").WithMessage("Senha deverá conter no mínimo uma letra minúscula, uma maiúscula, um número, um caractere especial e com o comprimento mínimo de oito caracteres.");
 			RuleFor(u => u.BirthDate)
 				.NotEmpty().WithMessage("Aniversário não pode ser vazio")
 				.LessThan(DateTime.Today.AddYears(-MINIMUM_AGE)).WithMessage("Aniversário precisa ser valido");
@@ -23,5 +24,5 @@ namespace yenoMoG.Armura.Domain.Validators
 				.NotEmpty().WithMessage("Gênero não pode ser vazio")
 				.Must(g => g.Equals('F') || g.Equals('M') || g.Equals('O'));
 		}
-    }
+	}
 }
--- test/yenoMoG.Armura.Test/Domain/Models/UserTest.cs
+++ test/yenoMoG.Armura.Test/Domain/Models/UserTest.cs
@@ -14,7 +14,7 @@ namespace yenoMoG.Armura.Test.Domain.Models
 		private const string PASSWORD = "Homolog@145";
 		private readonly DateTime BIRTH_DATE = new DateTime(2000,01,01);
 		private const char GENDER = 'M';
-		
+
 		[Fact]
 		public void Should_ReturnValidUser()
 		{
@@ -34,7 +34,7 @@ namespace yenoMoG.Armura.Test.Domain.Models
         [InlineData("788736qf856")]
         [InlineData("788736578564359")]
 		[InlineData("456.545.675-64")]
-		public void Should_ReturnFailWhenCpfNotValid(string cpf)
+		public void Should_ReturnFailWhenCpfInvalid(string cpf)
 		{
 			var user = User.Create(cpf, NAME, NICKNAME, EMAIL, PASSWORD, BIRTH_DATE, GENDER);
 			user.Should().NotBeNull();
@@ -43,16 +43,56 @@ namespace yenoMoG.Armura.Test.Domain.Models
 		}
 
 		[Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData(" ")]
+		public void Should_ReturnFailWhenNameNotEmpty(string name)
+        {
+            var user = User.Create(CPF, name, NICKNAME, EMAIL, PASSWORD, BIRTH_DATE, GENDER);
+            user.Should().NotBeNull();
+            user.IsFailure.Should().BeTrue();
+            user.Messages.Keys.Should().Contain("NameIsNotNullOrEmpty");
+            user.Messages.Should().ContainValues("O nome deve ser preenchido");
+        }
+
+        [Theory]
+        [InlineData("José")]
+		[InlineData("José S")]
+		[InlineData("José Da Siva Pereira R")]
+		[InlineData("Mariana")]
+        public void Should_ReturnFailWhenNameRegexInvalid(string name)
+        {
+            var user = User.Create(CPF, name, NICKNAME, EMAIL, PASSWORD, BIRTH_DATE, GENDER);
+            user.Should().NotBeNull();
+            user.IsFailure.Should().BeTrue();
+            user.Messages.Keys.Should().Contain("NameRegexInvalid");
+            user.Messages.Should().ContainValues("Nome deve ser completo");
+        }
+
+		[Theory]
 		[InlineData(null)]
 		[InlineData("")]
 		[InlineData(" ")]
-		[InlineData("José")]
-		public void Should_ReturnFailWhenNameNotValid(string name)
-		{
-			var user = User.Create(CPF, name, NICKNAME, EMAIL, PASSWORD, BIRTH_DATE, GENDER);
-			user.Should().NotBeNull();
-			user.IsFailure.Should().BeTrue();
-			user.Messages.Should().ContainValues("O CPF é invalido");
-		}
-	}
+		public void Should_ReturnFailWhenNicknameInvalid(string nickname)
+        {
+            var user = User.Create(CPF, NAME, nickname, EMAIL, PASSWORD, BIRTH_DATE, GENDER);
+            user.Should().NotBeNull();
+            user.IsFailure.Should().BeTrue();
+            user.Messages.Should().ContainValues("Apelido não pode ser vazio");
+        }
+
+		[Theory]
+		[InlineData(null)]
+		[InlineData("")]
+		[InlineData("r.c.e@.e#.com")]
+		[InlineData("-@a@glob.com")]
+		[InlineData("( ͡° ͜ʖ ͡°)@mailinator.com.br")]
+		public void Should_ReturnFailWhenEmailInvalid(string email)
+        {
+            var user = User.Create(CPF, NAME, NICKNAME, email, PASSWORD, BIRTH_DATE, GENDER);
+            user.Should().NotBeNull();
+            user.IsFailure.Should().BeTrue();
+            user.Messages.Should().ContainValues("Precisa conter email válido");
+        }
+    }
 }
